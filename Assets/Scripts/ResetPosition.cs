5b3c341fd68169e9ebd1e29bdd4cc3dda6939663
regevshema <107068774+regevshema@users.noreply.github.com>
2025-04-11 16:44:46 +0300

    ex4_mario

---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    private Vector3 startPositon;
+    private void OnEnable()
+    {
+        SC_Death.OnSpikeCollision += OnSpikeCollision;
+    }
+    private void OnDisable()
+    {
+        SC_Death.OnSpikeCollision -= OnSpikeCollision;
+    }
+    void Awake()
+    {
+        startPositon = transform.position;
+    }
+
+    private void OnSpikeCollision()
+    {
+        transform.position = startPositon;
+    }
+}
